7f3ea42b1c6be591168037b7b8e7603a4e812d9b
bitfriend <bitcapture@hotmail.com>
2022-07-09 05:13:31 -0700

    Build for ios

--- native/effektio/src/platform/ios.rs
+++ native/effektio/src/platform/ios.rs
@@ -2,10 +2,6 @@ use anyhow::Result;
 use log::LevelFilter;
 use matrix_sdk::ClientBuilder;
 use oslog::OsLogger;
-use sanitize_filename_reader_friendly::sanitize;
-use std::{fs, path};
-use tracing_subscriber::layer::SubscriberExt;
-use tracing_subscriber::{fmt::format::FmtSpan, EnvFilter};
 
 use super::native;
 
@@ -15,10 +11,36 @@ pub fn new_client_config(base_path: String, home: String) -> Result<ClientBuilde
 
 pub fn init_logging(filter: Option<String>) -> Result<()> {
     // FIXME: not yet supported
-    OsLogger::new("effektio-sdk")
-        .level_filter(LevelFilter::Debug)
-        .category_level_filter("Settings", LevelFilter::Trace)
-        .init()
-        .unwrap();
+    match filter.as_deref() {
+        Some("info") => {
+            OsLogger::new("effektio-sdk")
+                .level_filter(LevelFilter::Info)
+                .category_level_filter("Settings", LevelFilter::Trace)
+                .init()
+                .unwrap();
+        }
+        Some("debug") => {
+            OsLogger::new("effektio-sdk")
+                .level_filter(LevelFilter::Debug)
+                .category_level_filter("Settings", LevelFilter::Trace)
+                .init()
+                .unwrap();
+        }
+        Some("warn") => {
+            OsLogger::new("effektio-sdk")
+                .level_filter(LevelFilter::Warn)
+                .category_level_filter("Settings", LevelFilter::Trace)
+                .init()
+                .unwrap();
+        }
+        Some("error") => {
+            OsLogger::new("effektio-sdk")
+                .level_filter(LevelFilter::Error)
+                .category_level_filter("Settings", LevelFilter::Trace)
+                .init()
+                .unwrap();
+        }
+        _ => {}
+    }
     Ok(())
 }
